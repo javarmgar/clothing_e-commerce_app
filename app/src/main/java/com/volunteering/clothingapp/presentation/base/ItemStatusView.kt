bf57fa6c2eee0ebd503edfbb84558778db0a52ff
Javier Armenta <ja0822@dexcom.com>
2023-09-01 18:10:46 -0600

    adding layout app/src/main/res/layout/item_card_cloth_details.xml

--- app/src/main/java/com/volunteering/clothingapp/presentation/base/ItemStatusView.kt
+++ app/src/main/java/com/volunteering/clothingapp/presentation/base/ItemStatusView.kt
@@ -2,7 +2,6 @@ package com.volunteering.clothingapp.presentation.base
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
